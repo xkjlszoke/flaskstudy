717d358c407356094d1ec83be3e54fb1bef3a026
xkjlszoke <329213787@qq.com>
2017-08-30 18:36:41 +0800

    study day 1, 3 lessons

--- test.py
+++ test.py
@@ -1,10 +1,53 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from functools import wraps
 
 app = Flask(__name__)
 
+app.secret_key = 'wrajj forever'
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first!')
+            return redirect(url_for('login'))
+    return wrap
+
+
 @app.route('/')
-def hello_world():
-    return 'wrajj_forever'
+@login_required
+def home():
+    return render_template('index.html')
+
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            flash('you were just logged in!')
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('you were just logged out!')
+    return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
